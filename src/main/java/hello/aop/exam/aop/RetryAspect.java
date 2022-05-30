6777f804c12188ed2256bde0fa27626614f9fe3d
cares0 <cares00000@gmail.com>
2022-05-30 19:30:03 +0900

    스프링 AOP 실전 예제 - 재시도

---
+++ src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -0,0 +1,32 @@
+package hello.aop.exam.aop;
+
+import hello.aop.exam.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+    // 이러면 자동으로 hello.aop.exam.annotation.Retry가 들어간 셈임
+    // 파라미터로 Retry 어노테이션을 받고 있기 때문
+    @Around("@annotation(retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {} retry={}", joinPoint.getSignature(), retry);
+
+        int maxRetry = retry.value();
+        Exception exceptionHolder = null;
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count={}/{}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            } catch (Exception e) {
+                exceptionHolder = e;
+            }
+        }
+        throw exceptionHolder; //maxRetry만큼 시도했는데도 예외가 발생할 경우 예외를 던짐
+    }
+}
